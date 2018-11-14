0b27b9dbe5d56c6a139d60fc025d9e4ebfcb898f
YJ Kim <yj@YJs-MacBook-Pro.local>
2018-11-15 10:34:39 +1300

    call Api for the movie list

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state={}
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  callApi = () => {
+    return fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=b2eeb8086c84342e951bcf03c78d3e2e')
+      .then( response => response.json())
+      .then( json => json.results)
+  }
+
+  getMovies = async () => {
+    const movies = await this.callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  renderMovies = () => {
+    // console.log(this.state.movies);
+    const movies = this.state.movies.map( movie =>
+      <h1>{movie.original_title}</h1>
+    );
+
+    return movies
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.movies ? this.renderMovies() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+export default App;
