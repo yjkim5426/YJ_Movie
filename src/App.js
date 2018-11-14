83083002893abe9b854aaa81d562d17b7aede94f
YJ Kim <yj@YJs-MacBook-Pro.local>
2018-11-15 10:47:08 +1300

    get movie posters

--- src/App.js
+++ src/App.js
@@ -22,9 +22,10 @@ class App extends Component {
   }
 
   renderMovies = () => {
-    // console.log(this.state.movies);
+    const posterPath = 'https://image.tmdb.org/t/p/w500/';
+    console.log(this.state.movies);
     const movies = this.state.movies.map( movie =>
-      <h1>{movie.original_title}</h1>
+      <><img src={posterPath + movie.poster_path}></img><h1>{movie.original_title}</h1></>
     );
 
     return movies
