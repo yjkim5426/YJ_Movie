61177f2b8dfedefe43c8c87bda32a4c9609d0e0a
YJ Kim <yj@YJs-MacBook-Pro.lan>
2018-11-21 15:08:02 +1300

    Revert "design - done"
    This reverts commit 1a0b6b674cf0d08e5c0754e73924703d453d9c20.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React, { Component } from "react";
-import "./App.css";
-import Movie from "./Movie";
+import React, { Component } from 'react';
+import './App.css';
+import Movie from "./Movie"
 
 class App extends Component {
-  state = {};
+  state={}
 
   componentDidMount() {
     this.getMovies();
   }
 
   callApi = () => {
-    return fetch(
-      "https://api.themoviedb.org/3/trending/movie/day?api_key=b2eeb8086c84342e951bcf03c78d3e2e"
-    )
-      .then(response => response.json())
-      .then(json => json.results)
+    return fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=b2eeb8086c84342e951bcf03c78d3e2e')
+      .then( response => response.json())
+      .then( json => json.results)
       .catch(err => console.log(err));
-  };
+  }
 
   getMovies = async () => {
     const movies = await this.callApi();
     this.setState({
       movies
-    });
-  };
+    })
+  }
 
   renderMovies = () => {
+    // const posterPrePath = 'https://image.tmdb.org/t/p/w500/';
+    console.log(this.state.movies);
+    // const movies = this.state.movies.map( movie =>
+    //   <>
+    //     <img src={posterPrePath + movie.poster_path}></img>
+    //     <h1>{movie.original_title}</h1>
+    //     <h2>{movie.release_date}</h2>
+    //     <p>{movie.overview}</p>
+    //   </>
+    // );
     const movies = this.state.movies.map(movie => {
       return (
         <Movie
@@ -38,13 +46,12 @@ class App extends Component {
       );
     });
     return movies;
-  };
+  }
 
   render() {
-    const { movies } = this.state;
     return (
-      <div className={movies ? "app" : "appLoading"}>
-        {movies ? this.renderMovies() : "Loading"}
+      <div>
+        {this.state.movies ? this.renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,28 +1,37 @@
-import React from "react";
-import './Movie.css';
+import React from 'react';
 
-function Movie({ posterPath, title, releaseDate, overview }) {
+function Movie({posterPath, title, releaseDate, overview}){
   return (
-    <div className="movieCard">
-      <div className="moviePoster">
+    /* <h1>{posterPath}</h1>
+    <h1>{title}</h1>
+    <h1>{releaseDate}</h1>
+    <h1>{overview}</h1> */
+    <div className='movieCard'>
+      <div className='moviePoster'>
         <MoviePoster poster={posterPath} alt={title} />
       </div>
-      <div className="movieTitle">
+      <div className='movieTitle'>
         <h1>{title}</h1>
       </div>
-      <div className="movieReleaseDate">
+      <div className='movieReleaseDate'>
         <h3>{releaseDate}</h3>
       </div>
-      <div className="movieSummary">
+      <div className='movieSummary'>
         <p>{overview}</p>
       </div>
     </div>
-  );
+  )
 }
 
-function MoviePoster({ poster, alt }) {
-  const prePath = "https://image.tmdb.org/t/p/original/";
-  return <img className="posterImg" src={prePath + poster} alt={alt} />;
+function MoviePoster({poster, alt}) {
+  const prePath = 'https://image.tmdb.org/t/p/w500/';
+  return(
+    // <>
+    // {poster}
+    // {alt}
+    // </>
+    <img className='posterImg' src={prePath + poster} alt={alt} />
+  )
 }
 
-export default Movie;
+export default Movie
